4dbca0f96a44c3e8e98f375f4bb61b2c504b1ecf
celine228 <138981854+celine228@users.noreply.github.com>
2026-05-07 14:11:28 +0100

    Motor Thursday morning
    Refactor motor control and encoder handling for improved functionality.

--- Sensors/MotorControl.ino
+++ Sensors/MotorControl.ino
@@ -1,119 +1,175 @@
 #define Wire Wire1
 #include <Motoron.h>
 
-MotoronI2C mc;
+// ===== MOTOR CONTROLLERS =====
+// CHANGE addresses if needed!
+MotoronI2C mc1(16);   // Front motors
+MotoronI2C mc2(17);   // Rear motors
 
-// Encoder pins
-#define LEFT_ENC_A 2
-#define LEFT_ENC_B 4
-#define RIGHT_ENC_A 3
-#define RIGHT_ENC_B 5
+// ===== ENCODER PINS =====
+// Front Left
+#define FL_A 2
+#define FL_B 4
 
-volatile long leftCount = 0;
-volatile long rightCount = 0;
+// Front Right
+#define FR_A 3
+#define FR_B 5
 
-// Interrupt functions
-void leftEncoderISR()
-{
-  if (digitalRead(LEFT_ENC_B))
-    leftCount++;
-  else
-    leftCount--;
+// Rear Left
+#define RL_A 18
+#define RL_B 19
+
+// Rear Right
+#define RR_A 22
+#define RR_B 23
+
+// ===== ENCODER COUNTS =====
+volatile long countFL = 0;
+volatile long countFR = 0;
+volatile long countRL = 0;
+volatile long countRR = 0;
+
+// ===== ISR FUNCTIONS =====
+void ISR_FL() {
+  if (digitalRead(FL_B)) countFL++;
+  else countFL--;
 }
 
-void rightEncoderISR()
-{
-  if (digitalRead(RIGHT_ENC_B))
-    rightCount++;
-  else
-    rightCount--;
+void ISR_FR() {
+  if (digitalRead(FR_B)) countFR++;
+  else countFR--;
 }
 
-void stopMotors()
-{
-  mc.setSpeed(1,0);
-  mc.setSpeed(2,0);
+void ISR_RL() {
+  if (digitalRead(RL_B)) countRL++;
+  else countRL--;
 }
 
+void ISR_RR() {
+  if (digitalRead(RR_B)) countRR++;
+  else countRR--;
+}
+
+// ===== STOP ALL MOTORS =====
+void stopMotors() {
+  mc1.setSpeed(1, 0);  // FL
+  mc1.setSpeed(3, 0);  // FR
+  mc2.setSpeed(1, 0);  // RL
+  mc2.setSpeed(3, 0);  // RR
+}
+
+// ===== DRIVE FORWARD WITH CORRECTION =====
 void driveForward(int baseSpeed)
 {
-  leftCount = 0;
-  rightCount = 0;
-  // Error between wheels
-  long error = leftCount - rightCount;
+  // long leftAvg  = (countFL + countRL) / 2;
+  // long rightAvg = (countFR + countRR) / 2;
 
-  // Simple proportional correction
-  float Kp = 2.0;
-  int correction = Kp * error;
+  // long error = leftAvg - rightAvg;
 
-  int leftSpeed = baseSpeed - correction;
-  int rightSpeed = baseSpeed + correction;
+  // float Kp = 1.2;   // tune this
+  // int correction = Kp * error;
 
-  // Limit speeds
-  leftSpeed = constrain(leftSpeed, -800, 800);
-  rightSpeed = constrain(rightSpeed, -800, 800);
+  // int leftSpeed  = baseSpeed - correction;
+  // int rightSpeed = baseSpeed + correction;
 
-  mc.setSpeed(1, leftSpeed);
-  mc.setSpeed(2, rightSpeed);
+
+  // leftSpeed  = constrain(leftSpeed, -800, 800);
+  // rightSpeed = constrain(rightSpeed, -800, 800);
+
+  // Front board
+  mc1.setSpeed(1, baseSpeed);    // FL
+  mc1.setSpeed(3, -baseSpeed);   // FR
+
+  // Rear board
+  mc2.setSpeed(1, baseSpeed);    // RR
+  mc2.setSpeed(3, -baseSpeed);   // RL
 }
 
-void turnLeft(long targetTicks, int speed)
+// ===== STRAFE RIGHT =====
+void strafeRight(int speed)
 {
-  leftCount = 0;
-  rightCount = 0;
+  mc1.setSpeed(1, speed);    // FR
+  mc2.setSpeed(1, speed);   // FL
 
-  while (abs(leftCount)<targetTicks)
-  {
-    mc.setSpeed(1,-speed);
-    mc.setSpeed(2,speed);
-    delay(5);
-  }
-
-  stopMotors();
+  mc2.setSpeed(3, -speed);    // RL
+  mc1.setSpeed(3, -speed);   // RR
 }
 
-void turnRight(long targetTicks, int speed)
-{
-  leftCount = 0;
-  rightCount = 0;
+// ===== TURN LEFT =====
+//void turnLeft(long targetTicks, int speed)
+//{
+  //countFL = countFR = countRL = countRR = 0;
 
-  while (abs(rightCount)<targetTicks){
-    mc.setSpeed(1,speed);
-    mc.setSpeed(2,-speed);
-    delay(5);
-  }
+  //while (abs(countFL) < targetTicks)
+  //{
+    //mc1.setSpeed(1, -speed);
+    //mc1.setSpeed(3, speed);
 
-  stopMotors();
-}
+    //mc2.setSpeed(1, -speed);
+    //mc2.setSpeed(3, speed);
+  //}
 
+  //stopMotors();
+//}
+
+// ===== SETUP =====
 void setup()
 {
   Wire.begin();
 
-  mc.reinitialize();
-  mc.disableCrc();
-  mc.clearResetFlag();
+  // Init BOTH Motoron boards
+  mc1.reinitialize();
+  mc1.disableCrc();
+  mc1.clearResetFlag();
+
+  mc2.reinitialize();
+  mc2.disableCrc();
+  mc2.clearResetFlag();
 
   // Encoder pins
-  pinMode(LEFT_ENC_A, INPUT_PULLUP);
-  pinMode(LEFT_ENC_B, INPUT_PULLUP);
-  pinMode(RIGHT_ENC_A, INPUT_PULLUP);
-  pinMode(RIGHT_ENC_B, INPUT_PULLUP);
+  pinMode(FL_A, INPUT_PULLUP);
+  pinMode(FL_B, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(LEFT_ENC_A), leftEncoderISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_A), rightEncoderISR, CHANGE);
+  pinMode(FR_A, INPUT_PULLUP);
+  pinMode(FR_B, INPUT_PULLUP);
+
+  pinMode(RL_A, INPUT_PULLUP);
+  pinMode(RL_B, INPUT_PULLUP);
+
+  pinMode(RR_A, INPUT_PULLUP);
+  pinMode(RR_B, INPUT_PULLUP);
+
+  // Attach interrupts (A channels only)
+  attachInterrupt(digitalPinToInterrupt(FL_A), ISR_FL, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(FR_A), ISR_FR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RL_A), ISR_RL, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RR_A), ISR_RR, CHANGE);
 }
 
+// ===== MAIN LOOP =====
 void loop()
 {
-  for (int i = 0; i < 200; i++)
+  // Reset encoders
+  countFL = countFR = countRL = countRR = 0;
+
+  // ===== FORWARD =====
+  unsigned long start = millis();
+  while (millis() - start < 4000)
   {
     driveForward(400);
-    delay(20);
   }
 
   stopMotors();
   delay(1000);
-  turnLeft(500,300);
-  delay(1000);
+
+  // ===== TURN =====
+  //turnLeft(500, 300);
+  //delay(1000);
+
+  // ===== STRAFE =====
+  //strafeRight(400);
+  //delay(2000);
+
+  //stopMotors();
+  //delay(3000);
 }
