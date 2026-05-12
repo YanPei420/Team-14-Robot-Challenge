eb456965b1ffda24142386b74a32f2bed61ffafb
Naerthi <naerthi@hotmail.com>
2026-05-12 01:17:23 +0100

    Update RevivalButton_LED.ino
    added flashing code for when dead

--- Sensors/RevivalButton_LED.ino
+++ Sensors/RevivalButton_LED.ino
@@ -3,8 +3,16 @@ const int buttonPin = 17;
 const int redPin = 18;
 const int greenPin = 19;
 
+unsigned long lastFlash = 0;
+bool redState = false;
+
+bool deadSignal = false;
+
+unsigned long greenStart = 0;
+bool greenActive = false;
+
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP); // internal pull-up resistor
 
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -12,14 +20,37 @@ void setup() {
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
-
-  if (buttonState == HIGH) {
-    // Button pressed
-    digitalWrite(redPin, LOW);   // turn ON red
-    digitalWrite(greenPin, HIGH); // turn OFF green
-  } else {
-    // Button not pressed
-    digitalWrite(redPin, HIGH);  // turn OFF red
-    digitalWrite(greenPin, LOW); // turn ON green
+  
+  if (deadSignal){
+    if (millis() - lastFlash >= 300){
+      lastFlash = millis();
+      redState = !redState;
+      digitalWrite(redPin, redState);
+    }
+  	digitalWrite(greenPin, LOW);
+    return;
   }
+  
+  if (greenActive) {
+    if (millis() - greenStart < 3000) {
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, HIGH);
+      return;
+    } else {
+      greenActive = false;
+    }
+  }
+  
+  if (buttonState == LOW) {
+    // Button pressed
+    greenActive = true;
+    greenStart = millis();
+    
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+    return;
+  }
+  
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, LOW);
 }
